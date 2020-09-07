82855ba8958337b642e3d144900835c41ee795be
Bessy-Mukaria <bessymukaria@gmail.com>
2020-09-07 16:59:30 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Goal of this project is to implement machine learning model to predict customer churn.The importance of this is that customer retention is crucial to the long-term success of your business.',
+    author='Bessy Mukaria',
+    license='MIT',
+)
